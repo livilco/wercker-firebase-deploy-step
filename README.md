4c95586617887b0520d13d85eac67377008847f0
De Ville Weppenaar <deville.weppenaar@addynamo.com>
2016-08-30 11:07:27 +0200

    Add checking for arguments and debug mode

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ deploy command. Options are passed along to the `firebase` executable as is.
 
 * `project` - Firebase project name, as defined on the [Firebase console](https://console.firebase.google.com/), for example `lovely-llama`.
 * `token` - Firebase authentication token generated for use in non-interactive environments (see `login:ci` command for [Firebase CLI](https://firebase.google.com/docs/cli/)).
+* `debug` - (Optional) Prints out the command that is executed to the console. **Warning this prints everything in the clear, including the token.** Possible values are `true` or `false`. Defaults to `false`.
 
 # Example
 
@@ -23,6 +24,10 @@ deploy:
 The MIT License (MIT)
 
 # Changelog
+
+## 1.0.2
+
+- Add checking for arguments and debug mode
 
 ## 1.0.1
 
--- run.sh
+++ run.sh
@@ -1,3 +1,41 @@
 #!/bin/sh
 
-firebase deploy --project $WERCKER_FIREBASE_DEPLOY_PROJECT --token $WERCKER_FIREBASE_DEPLOY_TOKEN
+cli="firebase"
+
+main() {
+
+  if [ -z "$WERCKER_FIREBASE_DEPLOY_PROJECT" ]; then
+    fail "firebase-deploy: project argument cannot be empty"
+  fi
+
+	if [ -z "$WERCKER_FIREBASE_DEPLOY_TOKEN" ]; then
+    fail "firebase-deploy: token argument cannot be empty"
+  fi
+
+	# Command
+  cmd="deploy"
+
+  # Command arguments
+  args=
+
+	# project
+  if [ -n "$WERCKER_FIREBASE_DEPLOY_PROJECT" ]; then
+    args="$args --project \"$WERCKER_FIREBASE_DEPLOY_PROJECT\""
+  fi
+
+	# token
+  if [ -n "$WERCKER_FIREBASE_DEPLOY_TOKEN" ]; then
+    args="$args --token \"$WERCKER_FIREBASE_DEPLOY_TOKEN\""
+  fi
+
+	# Check if running in debug mode
+  info "Running firebase command"
+  if [ "$WERCKER_FIREBASE_DEPLOY_DEBUG" = "true" ]; then
+    info "$cli $cmd $args"
+  fi
+
+  eval "$cli" "$cmd" "$args"
+
+}
+
+main;
