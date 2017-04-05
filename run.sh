4547982e277d260c53d78af6bd8e87b3b0c8a5f1
De Ville Weppenaar <deville.weppenaar@addynamo.com>
2017-04-05 17:36:14 +0200

    work in progress, to be tested

--- run.sh
+++ run.sh
@@ -12,6 +12,10 @@ main() {
     fail "firebase-deploy: token argument cannot be empty"
   fi
 
+  if [ "$WERCKER_FIREBASE_DEPLOY_EXCEPT" ] && [ "$WERCKER_FIREBASE_DEPLOY_ONLY" ]; then
+    fail "firebase-deploy: cannot provide both \"except\" and \"only\" arguments"
+  fi
+
   # Command
   cmd="deploy"
 
@@ -26,6 +30,21 @@ main() {
   # token
   if [ -n "$WERCKER_FIREBASE_DEPLOY_TOKEN" ]; then
     args="$args --token \"$WERCKER_FIREBASE_DEPLOY_TOKEN\""
+  fi
+
+  # except
+  if [ -n "$WERCKER_FIREBASE_DEPLOY_EXCEPT" ]; then
+    args="$args --except \"$WERCKER_FIREBASE_DEPLOY_EXCEPT\""
+  fi
+
+  # only
+  if [ -n "$WERCKER_FIREBASE_DEPLOY_ONLY" ]; then
+    args="$args --only \"$WERCKER_FIREBASE_DEPLOY_ONLY\""
+  fi
+
+  # only
+  if [ -n "$WERCKER_FIREBASE_DEPLOY_MESSAGE" ]; then
+    args="$args --message \"$WERCKER_FIREBASE_DEPLOY_MESSAGE\""
   fi
 
   # Check if running in debug mode
